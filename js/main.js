db2c38102cb3248e1142fbd5a7f2efb9ba4edd39
josephalaa01 <joseph.alaa01@gmail.com>
2026-02-01 06:36:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ function checkPassword() {
     document.getElementById("error").innerText = "الباسورد غلط يا جميل ❤️";
   }
 }
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerText = "❤";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (Math.random() * 3 + 3) + "s";
+  document.body.appendChild(heart);
+
+  setTimeout(() => heart.remove(), 6000);
+}, 500);
+
