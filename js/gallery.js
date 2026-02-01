e195d49cd281efc40cc6ca6e90eba666cadb3804
josephalaa01 <joseph.alaa01@gmail.com>
2026-02-01 06:46:40 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,17 @@
-const upload = document.getElementById("upload");
+const dropZone = document.getElementById("dropZone");
 const gallery = document.getElementById("gallery");
 
-upload.addEventListener("change", () => {
-  [...upload.files].forEach(file => {
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(file);
-    gallery.appendChild(img);
+dropZone.ondragover = e => e.preventDefault();
+
+dropZone.ondrop = e => {
+  e.preventDefault();
+  [...e.dataTransfer.files].forEach(file => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = document.createElement("img");
+      img.src = reader.result;
+      gallery.appendChild(img);
+    };
+    reader.readAsDataURL(file);
   });
-});
+};
